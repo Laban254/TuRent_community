5a4c9cff393099fad7cadb0b3dd3689f6c9794c4
atonya-bravin <bravinatonya.brani@gmail.com>
2023-06-21 20:59:21 +0300

    removal of the disabling effect to the text boxes on click of the save_information button and also inclusion of a display of none to the edit button

--- app/static/scripts/edit_plot.js
+++ app/static/scripts/edit_plot.js
@@ -3,22 +3,15 @@ document.addEventListener("DOMContentLoaded", function() {
   const editButton = document.getElementById("edit_button");
   const saveButton = document.getElementById("save_button");
   const form = document.getElementById("edit_form");
-  const textFields = form.querySelectorAll("input[type='text']");
+  const textFields = document.querySelectorAll(".text_box");
 
   // Enable editing when the Edit Information button is clicked
   editButton.addEventListener("click", function() {
       for (const field of textFields) {
           field.removeAttribute("disabled");
       }
-      saveButton.removeAttribute("disabled");
+      editButton.style["display"]="none";
   });
 
-  // Disable editing and submit the form when the Save button is clicked
-  saveButton.addEventListener("click", function() {
-      for (const field of textFields) {
-          field.setAttribute("disabled", true);
-      }
-      saveButton.setAttribute("disabled", true);
-      form.submit();
-  });
+  
 });
