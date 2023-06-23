efb774ad91c0a9231d8e7892adbd2452a21bf108
atonya-bravin <bravinatonya.brani@gmail.com>
2023-06-23 14:17:14 +0300

    inclusion of the drag and drop feature

--- app/static/scripts/house_info.js
+++ app/static/scripts/house_info.js
@@ -1,49 +1,53 @@
+// prevent the default behavior of web browser
+['dragleave', 'drop', 'dragenter', 'dragover'].forEach(function (evt) {
+  document.addEventListener(evt, function (e) {
+      e.preventDefault();
+  }, false);
+});
 
-var dropzone = document.getElementById('dropzone');
-
-dropzone.addEventListener('dragenter', handleDragEnter, false);
-dropzone.addEventListener('dragover', handleDragOver, false);
-dropzone.addEventListener('dragleave', handleDragLeave, false);
-dropzone.addEventListener('drop', handleDrop, false);
-
-function handleDragEnter(e) {
+var drop_area = document.getElementById('drop_area');
+drop_area.addEventListener('drop', function (e) {
   e.preventDefault();
-  dropzone.classList.add('hover');
-}
-
-function handleDragOver(e) {
-  e.preventDefault();
-}
-
-function handleDragLeave(e) {
-  e.preventDefault();
-  dropzone.classList.remove('hover');
-}
-
-function handleDrop(e) {
-  e.preventDefault();
-  dropzone.classList.remove('hover');
-
-  var files = e.dataTransfer.files;
-  if (files.length > 0) {
-    var file = files[0];
-    var reader = new FileReader();
-
-    reader.onload = function(e) {
-      var img = document.createElement('img');
-      img.src = e.target.result;
-      img.classList.add('dropped-image');
-
-      // Remove existing dropped images, if any
-      var existingImages = dropzone.querySelectorAll('.dropped-image');
-      for (var i = 0; i < existingImages.length; i++) {
-        dropzone.removeChild(existingImages[i]);
-      }
-
-      dropzone.appendChild(img);
-      dropzone.querySelector('.dropzone-text').style.display = 'none';
-    };
-
-    reader.readAsDataURL(file);
+  var fileList = e.dataTransfer.files; // the files to be uploaded
+  if (fileList.length == 0) {
+      return false;
   }
-}
+
+  // we use XMLHttpRequest here instead of fetch, because with the former we can easily implement progress and speed.
+  var xhr = new XMLHttpRequest();
+  xhr.open('post', '/upload', true); // aussume that the url /upload handles uploading.
+  xhr.onreadystatechange = function () {
+      if (xhr.readyState == 4 && xhr.status == 200) {
+          // uploading is successful
+          alert('Successfully uploaded!');  // please replace with your own logic
+      }
+  };
+
+  // show uploading progress
+  var lastTime = Date.now();
+  var lastLoad = 0;
+  xhr.upload.onprogress = function (event) {
+      if (event.lengthComputable) {
+          // update progress
+          var percent = Math.floor(event.loaded / event.total * 100);
+          document.getElementById('upload_progress').textContent = percent + '%';
+
+          // update speed
+          var curTime = Date.now();
+          var curLoad = event.loaded;
+          var speed = ((curLoad - lastLoad) / (curTime - lastTime) / 1024).toFixed(2);
+          document.getElementById('speed').textContent = speed + 'MB/s'
+          lastTime = curTime;
+          lastLoad = curLoad;
+      }
+  };
+
+  // send files to server
+  xhr.setRequestHeader('X-Requested-With', 'XMLHttpRequest');
+  var fd = new FormData();
+  for (let file of fileList) {
+      fd.append('files', file);
+  }
+  lastTime = Date.now();
+  xhr.send(fd);
+}, false);
